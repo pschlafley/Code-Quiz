bef2b151edae00a95c8a398560d4b2de44dc82d5
Peyton Schlafley <pschlafley0@gmail.com>
2020-08-30 13:22:59 -0500

    worked on for loop

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,11 @@
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
 var startBtn = document.getElementById("start-button");
 var timerCountdown = document.getElementById("quiz-timer");
 var answerList = document.getElementById("answer-list");
@@ -8,8 +16,8 @@ var currentQuestion = 0;
 var questions = [
     {
         question: "Question 1: What color is the sky?",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        choices: ["blue", "green", "red", "orange"],
+        correct: "blue"
     },
     {
         question: "Question 2: What color is the grass?",
@@ -33,6 +41,7 @@ var questions = [
     },
 ];
 
+
 function startQuiz() {
     quizTimer();
 };
@@ -41,7 +50,7 @@ function startQuiz() {
 
 // timer starts when button is clicked
 function quizTimer() {
-    var timeLeft = 60;
+    var timeLeft = 59;
 
     var timeInterval = setInterval(function() {
         if (timeLeft >= 0) {
@@ -61,25 +70,19 @@ function quizTimer() {
   function getQuestion() {
     var question = questions[currentQuestion];
     document.getElementById("question").textContent = question.question;
+    
     for (i = 0; i < question.choices.length; i++) {
         var listItemEl = document.createElement("li");
         listItemEl.textContent = question.choices[i];
         answerList.appendChild(listItemEl);
     }    
+    currentQuestion++;
+    currentQuestion.choices++;
 };
 
- // when user answers a question then the next question appears
-
-
+ 
 // click the start button to run the quizTimer function
 startBtn.addEventListener("click", startQuiz);
 
-
-
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+// make the created "li" clickable
+answerList.addEventListener("click", getQuestion);
