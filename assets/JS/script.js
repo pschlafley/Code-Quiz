f81390991d1d1a6e9b2a5004c8b162bf05cc4a15
Peyton Schlafley <pschlafley0@gmail.com>
2020-08-30 20:01:36 -0500

    when user clicks a question the next question set appears

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,49 +10,50 @@ var startBtn = document.getElementById("start-button");
 var timerCountdown = document.getElementById("quiz-timer");
 var answerList = document.getElementById("answer-list");
 var quizSection = document.getElementById("quiz-wrapper");
-
+var timeLeft = 59;
+var timeInterval;
+var score = timeLeft;
 var currentQuestion = 0;
 
 var questions = [
     {
-        question: "Question 1: What color is the sky?",
-        choices: ["blue", "green", "red", "orange"],
-        correct: "blue"
+        question: "Question 1: How do you write 'Hello World' in an alert box?",
+        choices: ["msg('Hello World');", "confirm('Hello World');", "alert('Hello World');", "prompt('Hello World');"],
+        correct: "alert('Hello World');"
     },
     {
-        question: "Question 2: What color is the grass?",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 2: How can you add a comment in JavaScript?",
+        choices: ["'This is a comment", "/* This is a comment */", "<!-- This is a comment -->", "// This is a comment"],
+        correct: "// This is a comment"
     },
     {
-        question: "Question 3: The current year is _____.?",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 3: How can you write a Javascript array?",
+        choices: ["var color = 1 = ('red'), 2=('green')", "var colors = ['red', 'green']", "var colors = 'red', 'green'", "var colors = (1:'red', 2:'green')"],
+        correct: "var colors = ['red', 'green']"
     },
     {
-        question: "Question 4: What noise does a dog make?",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 4: Which event occurs when the user clicks on the HTML element?",
+        choices: ["onclick", "submit", "keypress", "wheel"],
+        correct: "onclick"
     },
     {
-        question: "Question 5: How many days are in a year?",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 5: Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+        choices: ["last()", "push()", "put()", "none of the above"],
+        correct: "push()"
     },
 ];
 
-
 function startQuiz() {
     quizTimer();
+    getQuestion();
+    userPick();
 };
-
 
 
 // timer starts when button is clicked
 function quizTimer() {
-    var timeLeft = 59;
 
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
         if (timeLeft >= 0) {
             timerCountdown.textContent = timeLeft;
             timeLeft --;
@@ -62,8 +63,6 @@ function quizTimer() {
     }, 1000);
 
     document.getElementById("quiz-timer").style.color = "red";
-
-    getQuestion();
 };
 
 // when button is clicked then questions and answers appear
@@ -72,18 +71,40 @@ function quizTimer() {
 
     document.getElementById("question").textContent = question.question;
 
-    document.getElementById("answer-list").textContent = question.choices;
+    answerList.textContent = question.choices[i];
 
-    for (i = 0; i < question.choices.length; i++) {
+    for (var i = 0; i < question.choices.length; i++) {
         var listItemEl = document.createElement("li");
         listItemEl.textContent = question.choices[i];
         answerList.appendChild(listItemEl);
     }    
     currentQuestion++;
-    currentQuestion.choices++;
+    currentQuestion.choices++;    
 };
 
- 
+function userPick() {
+        var answer = question.correct;
+
+        if (answer) {
+            var alertCorrect = document.createElement("span");
+            alertCorrect.textContent = "Correct!"
+            var userScore = score + 10;
+            userScore++;
+        } else {
+            var alertWrong = document.createElement("span");
+            alertWrong.textContent = "Wrong";
+            timeLeft = timeLeft - 10;
+            timeLeft--; 
+        }
+};
+
+function endQuiz() {
+    // when time hits zero end the quiz
+    clearInterval(timeInterval);
+    
+};
+
+
 // click the start button to run the quizTimer function
 startBtn.addEventListener("click", startQuiz);
 
